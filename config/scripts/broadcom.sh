e1d05bbfc8adff3cab68b620069176c4f6857da7
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-09 17:14:15 -0300

    setup ublue-mac image

---
+++ config/scripts/broadcom.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Enable broadcom wifi chip'
+rm -R /etc/modprobe.d/broadcom-wl-blacklist.conf
+rm -R /etc/modprobe.d/default-disable-broadcom-wl.conf
