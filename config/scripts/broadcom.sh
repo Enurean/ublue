35095da137b776f98e3377a132fb60e04bc7f82c
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-10 07:45:32 -0300

    update wl script

--- config/scripts/broadcom.sh
+++ config/scripts/broadcom.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
+# Your code goes here.
+echo 'Enable broadcom wifi chip'
+rpm-ostree install /tmp/rpms/kmods/*wl*.rpm
+rm /etc/modprobe.d/broadcom-wl-blacklist.conf
+rm /etc/modprobe.d/default-disable-broadcom-wl.conf
+
 # Tell this script to exit if there are any errors.
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
 set -oue pipefail
-
-# Your code goes here.
-echo 'Enable broadcom wifi chip'
-rpm-ostree install /tmp/rpms/kmods/*wl*.rpm
-# rm /etc/modprobe.d/broadcom-wl-blacklist.conf
-# rm /etc/modprobe.d/default-disable-broadcom-wl.conf
