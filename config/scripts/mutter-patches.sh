2f28ed3f6691ca43fd54dc4e81c94d80b945814b
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-10 16:42:45 -0300

    add mutter-patches

---
+++ config/scripts/mutter-patches.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Override default packages
+echo 'Install mutter patches'
+sudo rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter xorg-x11-server-Xwayland
