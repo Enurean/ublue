9666c943f2ee6e106748f63147c38c324d54dc3f
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-10 08:11:36 -0300

    fix path

--- config/scripts/broadcom.sh
+++ config/scripts/broadcom.sh
@@ -3,8 +3,8 @@
 # Your code goes here.
 echo 'Enable broadcom wifi chip'
 rpm-ostree install /tmp/rpms/kmods/*wl*.rpm
-rm /etc/modprobe.d/broadcom-wl-blacklist.conf
-rm /etc/modprobe.d/default-disable-broadcom-wl.conf
+rm system_files/usr/etc/modprobe.d/broadcom-wl-blacklist.conf
+rm system_files/usr/etc/modprobe.d/default-disable-broadcom-wl.conf
 
 # Tell this script to exit if there are any errors.
 # You should have this in every custom script, to ensure that your completed
