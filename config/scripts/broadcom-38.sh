4cc8cf7edf94750013e6e4d9ed90c78b6816e9ca
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-12 17:23:21 -0300

    rebase ublue-mac to 38

---
+++ config/scripts/broadcom-38.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Enable broadcom wifi chip
+echo 'Enable broadcom wifi chip'
+rpm-ostree install /tmp/rpms/kmods/*wl*.rpm
+rm /usr/etc/modprobe.d/broadcom-wl-blacklist.conf
+rm /usr/etc/modprobe.d/default-disable-broadcom-wl.conf
