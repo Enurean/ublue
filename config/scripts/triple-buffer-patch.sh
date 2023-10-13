32f4d16b70aeac827891c4e1b6b19e3a7576f99b
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-13 19:26:27 -0300

    fix override command

--- config/scripts/triple-buffer-patch.sh
+++ config/scripts/triple-buffer-patch.sh
@@ -7,4 +7,4 @@ set -oue pipefail
 
 # Install triple-buffer-patch
 echo 'Install triple-buffer-patch'
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common xorg-x11-server-Xwayland
+sudo rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:trixieua:mutter-patched gnome-shell mutter mutter-common xorg-x11-server-Xwayland
