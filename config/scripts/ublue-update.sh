ef1dc990e0ce0cd019158cca3f334f0c62e798df
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-18 11:28:58 -0300

    fix: copy config

--- config/scripts/ublue-update.sh
+++ config/scripts/ublue-update.sh
@@ -9,5 +9,5 @@ set -oue pipefail
 echo 'Set AutomaticUpdatePolicy to None'
 sed -i '/^AutomaticUpdatePolicy/s/stage/none/' /etc/rpm-ostreed.conf
 
-echo 'Turn notifications off'
-sed -i '/^dbus_notify/s/true/false/ ' /etc/ublue-update/ublue-update.toml
+# echo 'Turn notifications off'
+# sed -i '/^dbus_notify/s/true/false/ ' /etc/ublue-update/ublue-update.toml
