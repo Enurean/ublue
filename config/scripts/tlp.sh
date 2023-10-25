a956ddf8f51ba6d111c9586baab8eb6838e06cdb
Enurean <137900552+Enurean@users.noreply.github.com>
2023-10-25 09:03:51 -0300

    setup config

---
+++ config/scripts/tlp.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Configure tlp
+echo 'Configure tlp'
+systemctl enable tlp.service
+systemctl mask systemd-rfkill.service systemd-rfkill.socket
